74edc7f72f9a34b4ed355c09f4095ca8b2460690
arya satwika <aryastwk95@gmail.com>
2025-11-06 16:18:22 +0700

    init

---
+++ test/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'test';
+
+    $connection = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+    if ($connection) {
+        echo "Koneksi Berhasil";
+    }
+    else {
+        echo "Koneksi Gagal: " . mysqli_connect_error();
+    }
+?>
---
+++ test/register.php
@@ -0,0 +1,48 @@
+<?php
+    include 'koneksi.php';
+
+    if (isset($_POST['button'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+        mysqli_query($connection, $query);
+    }
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="loginpage.css">
+    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/500px-Facebook_Logo_%282019%29.png" type="image/x-icon">
+    <title>facebook</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <?php
+        include 'pup.html';
+    ?>
+    <div class="container">
+        <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Facebook_Logo_%282019%29.svg/300px-Facebook_Logo_%282019%29.svg.png" alt="facebook">
+        <p id="desc">
+            Facebook helps you connect and share with the people in your life.
+        </p>
+        <div class="login-container">
+        <form action="register.php" method="POST">
+            <input class ="input-field" type="text" name="username" placeholder="Email or Phone Number">
+            <br>
+            <input class ="input-field" type="password" name="password" placeholder="Password">
+            <br>
+            <button name="button" type="submit">Register</button>
+            <a href="" class="forgor">Forgotten Password?</a>
+        </form>
+        <hr>
+        <button class="create">Create New Account</button>
+        </div>
+        <p id="footer"><a id="footer-link">Create a Page</a> for a celebrity, brand or business.</p>
+    </div>
+</body>
+</html>
